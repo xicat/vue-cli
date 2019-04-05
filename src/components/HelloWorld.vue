2d9e78ed22b1f596f026eb5f38c9a4d7896f2221
arvin20180802 <arvin001@126.com>
2019-04-05 23:26:35 +0800

    mock

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <span>{{ str }}</span>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +36,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      str: '1234567'
+    }
   }
 }
 </script>
